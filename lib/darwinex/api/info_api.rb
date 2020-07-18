145587e956620fbd02c637f7c9e63fc5bac5115a
James Frost <hello@jamesfrost.me>
2020-07-18 20:07:07 +0100

    Finished adding the Info Api endpoints

--- lib/darwinex/api/info_api.rb
+++ lib/darwinex/api/info_api.rb
@@ -30,12 +30,103 @@ module Darwinex::Api
       send('get', '/products', options.merge(query))
     end
 
+    def get_candles(product_name, resolution: nil, from:, to:)
+      query = {
+        query: {
+          resolution: resolution,
+          from: from,
+          to: to
+        }
+      }
+
+      send('get', "/products/#{product_name}/candles", options.merge(query))
+    end
+
+    def get_dxscore(product_name)
+      send('get', "/products/#{product_name}/dxscore", options)
+    end
+
+    def get_badges(product_name)
+      send('get', "/products/#{product_name}/history/badges", options)
+    end
+
+    def get_close_strategy(product_name)
+      send('get', "/products/#{product_name}/history/closestrategy", options)
+    end
+
+    def get_duration_consistency(product_name)
+      send('get', "/products/#{product_name}/history/durationconsistency", options)
+    end
+
+    def get_experience(product_name)
+      send('get', "/products/#{product_name}/history/experience", options)
+    end
+
+    def get_losing_consistency(product_name)
+      send('get', "/products/#{product_name}/history/losingconsistency", options)
+    end
+
+    def get_market_correlation(product_name)
+      send('get', "/products/#{product_name}/history/marketcorrelation", options)
+    end
+
+    def get_performance(product_name)
+      send('get', "/products/#{product_name}/history/performance", options)
+    end
+
+    def get_open_strategy(product_name)
+      send('get', "/products/#{product_name}/history/openstrategy", options)
+    end
+
+    def get_capacity(product_name)
+      send('get', "/products/#{product_name}/history/capacity", options)
+    end
+
+    def get_quotes(product_name, from: nil, to: nil)
+      query = {
+        query: {
+          start: from,
+          end: to
+        }
+      }
+
+      send('get', "/products/#{product_name}/history/quotes", options.merge(query))
+    end
+
+    def get_risk_adjustment(product_name)
+      send('get', "/products/#{product_name}/history/riskadjustment", options)
+    end
+
+    def get_risk_stability(product_name)
+      send('get', "/products/#{product_name}/history/riskstability", options)
+    end
+
+    def get_winning_consistency(product_name)
+      send('get', "/products/#{product_name}/history/winningconsistency", options)
+    end
+
+    def get_order_divergence(product_name)
+      send('get', "/products/#{product_name}/history/orderdivergence", options)
+    end
+
+    def get_return_divergence(product_name)
+      send('get', "/products/#{product_name}/history/returndivergence", options)
+    end
+
+    def get_monthly_divergence(product_name)
+      send('get', "/products/#{product_name}/monthlydivergence", options)
+    end
+
     def get_product_status(product_name)
       send('get', "/products/#{product_name}/status", options)
     end
 
     def get_product_scores(product_name)
       send('get', "/products/#{product_name}/scores", options)
+    end
+
+    def get_product_scores_badge(product_name, badge)
+      send('get', "/products/#{product_name}/scores/#{badge}", options)
     end
 
     private
--- lib/darwinex/product.rb
+++ lib/darwinex/product.rb
@@ -5,13 +5,95 @@ module Darwinex
       @info_api = info_api
     end
 
+    def candles(resolution: nil, from:, to:)
+      info_api.get_candles(
+        product_name,
+        resolution: resolution,
+        from: from,
+        to: to
+      )
+    end
+
+    def dxscore
+      info_api.get_dxscore(product_name)
+    end
+
+    def badges
+      info_api.get_badges(product_name)
+    end
+
+    def close_strategy
+      info_api.get_close_strategy(product_name)
+    end
+
+    def duration_consistency
+      info_api.get_duration_consistency(product_name)
+    end
+
+    def experience
+      info_api.get_experience(product_name)
+    end
+
+    def losing_consistency
+      info_api.get_losing_consistency(product_name)
+    end
+
+    def market_correlation
+      info_api.get_market_correlation(product_name)
+    end
+
+    def performance
+      info_api.get_performance(product_name)
+    end
+
+    def open_strategy
+      info_api.get_open_strategy(product_name)
+    end
+
+    def capacity
+      info_api.get_capacity(product_name)
+    end
+
+    def quotes(from: nil, to: nil)
+      info_api.get_quotes(product_name, from: from, to: to)
+    end
+
+    def risk_adjustment
+      info_api.get_risk_adjustment(product_name)
+    end
+
+    def risk_stability
+      info_api.get_risk_stability(product_name)
+    end
+
+    def winning_consistency
+      info_api.get_winning_consistency(product_name)
+    end
+
+    def order_divergence
+      info_api.get_order_divergence(product_name)
+    end
+
+    def return_divergence
+      info_api.get_return_divergence(product_name)
+    end
+
+    def monthly_divergence
+      info_api.get_monthly_divergence(product_name)
+    end
+
     def status
       info_api.get_product_status(product_name)
     end
 
-    def scores
-      info_api.get_product_scores(product_name)
+    def scores(badge: nil)
+      if badge.nil?
+        info_api.get_product_scores(product_name)
+      else
+        info_api.get_product_scores_badge(product_name, badge)
+      end
     end
+
 
     private
 
