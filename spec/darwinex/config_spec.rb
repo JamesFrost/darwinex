b1403289d4efce3eac33e8e268045e0b723fcfab
James Frost <hello@jamesfrost.me>
2020-07-19 17:01:19 +0100

    Added config spec

---
+++ spec/darwinex/config_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Darwinex::Config do
+  subject(:config) do
+    described_class.new(
+      token_api: token_api,
+      consumer_key: consumer_key,
+      consumer_secret: consumer_secret
+    )
+  end
+
+  let(:token_api) { instance_double('Darwinex::Api::TokenApi') }
+  let(:consumer_key) { 'consumer_key' }
+  let(:consumer_secret) { 'consumer_secret' }
+
+  describe '#refresh_access_token' do
+    let(:refresh_token) { 'xyz321' }
+
+    let(:refresh_access_token_args) do
+      {
+        refresh_token: refresh_token,
+        consumer_key: consumer_key,
+        consumer_secret: consumer_secret
+      }
+    end
+
+    let(:access_token) { 'access_token' }
+
+    let(:tokens) do
+      {
+        'access_token' => access_token,
+        'refresh_token' => 'new_refresh_token',
+        'scope' => 'openid',
+        'id_token' => 'a_long_string',
+        'token_type' => 'Bearer',
+        'expires_in' => 3600
+      }
+    end
+
+    before do
+      expect(token_api).to receive(:refresh_access_token).with(refresh_access_token_args).and_return(tokens)
+    end
+
+    it { expect(config.refresh_access_token(refresh_token)).to eq(tokens) }
+
+    it { expect { config.refresh_access_token(refresh_token) }.to change { config.access_token }.to(access_token) }
+  end
+end
